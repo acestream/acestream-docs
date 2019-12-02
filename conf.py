fcdfc65f8ba0e5c99d5b8ebdbb83ced5710a5213
Anton <apishel@gmail.com>
2019-12-02 14:34:54 +0200

    Use "sphinx_rtd_theme" theme

--- conf.py
+++ conf.py
@@ -16,11 +16,12 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
+import sphinx_rtd_theme
 
 # -- Project information -----------------------------------------------------
 
 project = u'Ace Stream'
-copyright = u'2018, Ace Stream authors'
+copyright = u'2019, Ace Stream authors'
 author = u'Ace Stream authors'
 
 # The short X.Y version
@@ -39,6 +40,7 @@ release = u''
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    "sphinx_rtd_theme",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -74,7 +76,7 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'default'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
